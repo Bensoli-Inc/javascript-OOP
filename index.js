894e70e2630b3dfa798dc6274dda75a538b59641
Bensoli-Inc <benswambua96@gmail.com>
2024-08-10 08:06:30 +0300

    enumrating

--- index.js
+++ index.js
@@ -33,5 +33,23 @@ function Cirrcle(radius) {
         console.log('draw');
     }
 }
+const circle1 = new Cirrcle(1);
+circle1.location = {x:1};
+delete circle1.location;
 
-const another = new Cirrcle(1);
+//primitive types: number, string boolean sympol undefined null
+let number = 10
+function increase(number) {
+    number++
+}
+increase(number);
+console.log(number) //get 10
+
+
+//reference type: objects functions arrays
+let numberObject = {value: 10}
+function increase(numberObject){
+    numberObject.value++
+}
+increase(numberObject);
+console.log(numberObject); //get11
